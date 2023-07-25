130f912aeccddb7cddb79facafc196b30fc3214f
Bruno <bvaleiro@gmail.com>
2023-07-25 06:55:57 -0300

    feature:first commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Routes } from './routes'
+import { FacebookLogo, InstagramLogo } from '@phosphor-icons/react'
+import LogoFafa from './assets/logoFafa.png'
+export function App() {
+  return (
+    <BrowserRouter>
+      <div className="mx-3  sm:mx-auto ">
+        <div className="flex flex-col justify-center items-center w-full">
+          <div className="w-full sm:w-[80rem]  ">
+            <div className="h-48 bg-white mb-2 flex justify-center items-center flex-col rounded-b-lg">
+              <img
+                src={LogoFafa}
+                alt="desenho de botões, agulha de croche e um texto fafa croche"
+                className="w-48 h-48"
+              />
+              <div className="flex justify-around items-center w-48  mb-4">
+                <a href="/">Bolsas</a>
+                <a href="/contato">Contato</a>
+              </div>
+            </div>
+            <Routes />
+            <div className="h-28 bg-white mb-2 rounded-lg flex justify-center items-center gap-12">
+              <InstagramLogo size={32} />
+              <FacebookLogo size={32} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: 'jit',
+  content: ["./src/**/*.tsx"],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
