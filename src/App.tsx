78d18a3f022986fc791e0c99b596776dba2866fa
Bruno <bvaleiro@gmail.com>
2023-11-01 22:16:26 -0300

    fix:erros

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState, useEffect } from 'react'
 import LogoFafa from './assets/logoFafa.png'
 import { stripe } from './lib/stripe'
@@ -103,24 +102,3 @@ export function App() {
     </div>
   )
 }
-
-/* <BrowserRouter>
-    <div className="mx-3  sm:mx-auto ">
-      <div className="flex flex-col justify-center items-center w-full">
-        <div className="w-full sm:w-[80rem]  ">
-          <div className="h-48 bg-white mb-2 flex justify-center items-center flex-col rounded-b-lg">
-            
-            <div className="flex justify-around items-center w-48  mb-4">
-              <Link to="/">Bolsas</Link>
-              <Link to="/contato">Contato</Link>
-            </div>
-          </div>
-          <Routes />
-          <div className="h-28 bg-white mb-2 rounded-lg flex justify-center items-center gap-12">
-            <InstagramLogo size={32} />
-            <FacebookLogo size={32} />
-          </div>
-        </div>
-      </div>
-    </div>
-  </BrowserRouter> */
